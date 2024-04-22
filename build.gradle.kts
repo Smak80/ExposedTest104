9eb8fc72d45327082aa4c02c8b168ff25c28f8a7
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2024-04-22 18:13:42 +0300

    Убраны лишние подключения

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:0.49.0")
     implementation("org.jetbrains.exposed:exposed-java-time:0.49.0")
     implementation("com.mysql:mysql-connector-j:8.3.0")
-    implementation("org.slf4j:slf4j-api:2.0.13")
     testImplementation(kotlin("test"))
 }
 
